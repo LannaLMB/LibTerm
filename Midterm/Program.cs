f7a31b5c1109f1342324a82f22b072f503f6097b
Lanna <Lanna_lmb@hotmail.com>
2017-04-27 15:04:34 -0400

    Added Validation Class, Added Method for Menu

--- Midterm/Program.cs
+++ Midterm/Program.cs
@@ -10,7 +10,43 @@ namespace Midterm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Team!");
+            // Change Console Color
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+
+            // Welcome Message
+            Console.WriteLine("Welcome to the Grand Library!\n");
+
+
+
+            // Declare Variables
+
+
+
+
+
+            // Call Get Option Method to Show Menu and Get User Input
+            GetOption();
+
+
+
+
+        }
+
+
+
+
+
+        // **********  Methods Begin Here  ********** \\
+
+        public static int GetOption()
+        {
+            int Option;
+            // User Chooses to Check In a Book, Check Out a Book, Donate a Book, or Search for a Book
+            Console.WriteLine("What Can We Help You With Today at The Grand Library?\n");
+            Console.Write("\t1.)\tCheck-Out a Book\n\t2.)\tReturn a Book\n\t3.)\tSearch for a Book by Title\n\t4.)\tSearch for a Book by Author\n\nPlease Type the Number Associated With The Option You'd Like\n   ----->  ");
+            Option = Validation.GetRange(1, 4);
+            return Option;
         }
     }
 }
---
+++ Midterm/Validation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    class Validation
+    {
+
+        public static int GetValidInt()
+        {
+       
+            int number;
+
+            // Validate Input
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine();
+
+                // Number Validation with Colors
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine();
+                Console.Write("You Must Select A Valid Number From The Menu:  --->   ");
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            }
+            return number;
+        }
+
+
+        // Method to Get Input and Validate That Number is Between 0 and 4.
+        public static int GetRange(int min, int max)
+        {
+
+            // Get a Valid Int from Previous Method
+            int number = GetValidInt();
+
+            while (number < min || number > max)
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"You MUST Enter a Number That is in Between {min} and {max}  --->   ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                number = GetValidInt();
+            }
+            return number;
+
+        }
+    }
+}
