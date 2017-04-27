cdf167f7c8347734a4cd98cafe3f7bb093e6c5fb
Lanna <Lanna_lmb@hotmail.com>
2017-04-27 13:35:15 -0400

    Added Abstract Class to Get Status, Added Class to Use Enum for Status Values, Started Methods in Main

---
+++ Midterm/StatusValues.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    // Declare Enum for Status of Book Values
+   enum StatusValues { Available, CheckedOut}
+
+}
