541997be8482e9db8b2afecaa18016f1fcda8e28
Lanna <Lanna_lmb@hotmail.com>
2017-04-28 12:05:34 -0400

    Added Functionality to Menu Choices

--- Midterm/Program.cs
+++ Midterm/Program.cs
@@ -40,14 +40,26 @@ namespace Midterm
             Console.WriteLine("Welcome to the Grand Library!\n");
 
 
-            // Declare Variables
+            // Start of Menu
+            Console.WriteLine("\nWhat Can We Help You With Today at The Grand Library?\n\n");
+
+
+            // Start of Continue Loop
+            while (true)
+            {
+                // Call Get Option Method to Show Menu and Get User Input
+                GetOption();
+                GetInfo();
 
 
 
-            // Call Get Option Method to Show Menu and Get User Input
-            GetOption();
+                // Continue Loop
+                if (!Validation.GetContinue())
+                {
 
-
+                    return;
+                }
+            }
         }
 
         
@@ -59,13 +71,12 @@ namespace Midterm
 
 
         // Method to Get User Option of What They Would Like to Do Within The Menu
-        public static int GetOption()
+        public static void GetOption()
         {
 
             int Option;
 
             // User Chooses to Check In a Book, Check Out a Book, Donate a Book, or Search for a Book
-            Console.WriteLine("What Can We Help You With Today at The Grand Library?\n");
             string[] MenuValues = { "Check-Out a Book", "Return a Book", "Search for a Book by Title", "Search for a Book by Author", "Donate a Book" };
             
             // Print List of Menu Values with Select Numbers
@@ -75,50 +86,90 @@ namespace Midterm
                 Console.WriteLine($"{i + 1}.)\t{MenuValues[i]}");
             }
 
-            Console.Write("\nPlease Type the Number Associated With The Option You'd Like\n\n   ----->  ");
+
+            // Get User Choice
+            Console.Write("\n\nPlease Type the Number Associated With The Option You'd Like\n\n   ----->  ");
 
 
-
-            // Genre Choice Output
+            // User Choice Output
             Option = Validation.GetRange(1, 5);
             Console.WriteLine();
             Console.WriteLine($"You Chose The Option To {MenuValues[Option - 1]}.");
             Console.WriteLine("-----------------------------------------------------------------------------\n");
-
-            return Option;
+            //Console.WriteLine(GetInfo());
         }
 
 
 
+        // Method to Allow User to Go Forward With Their Choice
+        public static void GetInfo()
+        {
+            int Option = 1;
+            string Choice = "";
 
-        //// Method to Get Book Status
-        //public static StatusValues GetBookStatus()
-        //{
+            if (Option == 1)
+            {
 
-        //    int choice = 0;
+                // Print List of Books Here
+                Console.WriteLine("Please Select a Book to Check Out From the List Above:\n");
+                Choice = Validation.GetValidString(Console.ReadLine().ToUpper());
+            }
 
-        //    StatusValues UserChoice = StatusValues.Available;
+            else if (Option == 2)
+            {
 
-        //    switch (choice)
-        //    {
-        //        case 1:
-        //            UserChoice = StatusValues.Available;
-        //            break;
+                Console.WriteLine("Please Type in The Title of The Book You Are Returning:\n");
+                Choice = Validation.GetValidString(Console.ReadLine().ToUpper());
+                Console.WriteLine("Thank You For Returning " + Choice);
+            }
 
-        //        case 2:
-        //            UserChoice = StatusValues.CheckedOut;
-        //            break;
+            else if (Option == 3)
+            {
 
-        //    }
+                Console.WriteLine("Please Type in The Name of The Book You Are Looking For:\n");
+                Choice = Validation.GetValidString(Console.ReadLine().ToUpper());
+            }
 
-        //    return UserChoice;
-        //}
+            else if (Option == 4)
+            {
+
+                Console.WriteLine("Please Type in The Author of The Book You Are Looking For:\n");
+                Choice = Validation.GetValidString(Console.ReadLine().ToUpper());
+            }
+
+            else if (Option == 5)
+            {
+
+                Console.WriteLine("Please Enter in The Information For The Book You Are Donating:\n");
+                Choice = Validation.GetValidString(Console.ReadLine().ToUpper());
+            }
+
+            //return Choice;
+        }
 
 
 
-        
+        // Method to Get Book Status
+        public static StatusValues GetBookStatus()
+        {
 
+            int choice = 0;
 
+            StatusValues UserChoice = StatusValues.Available;
+
+            switch (choice)
+            {
+                case 1:
+                    UserChoice = StatusValues.Available;
+                    break;
+
+                case 2:
+                    UserChoice = StatusValues.CheckedOut;
+                    break;
+            }
+
+            return UserChoice;
+        }
     }
 }
 
--- Midterm/Validation.cs
+++ Midterm/Validation.cs
@@ -9,9 +9,10 @@ namespace Midterm
     class Validation
     {
 
+        // Method To Get A Valid Integer
         public static int GetValidInt()
         {
-       
+
             int number;
 
             // Validate Input
@@ -24,13 +25,14 @@ namespace Midterm
                 Console.WriteLine();
                 Console.Write("You Must Select A Valid Number From The Menu:  --->   ");
                 Console.ForegroundColor = ConsoleColor.DarkBlue;
-
             }
+
             return number;
         }
 
 
-        // Method to Get Input and Validate That Number is Between 0 and 4.
+
+        // Method to Get Input and Validate That Number is Between 1 and 5.
         public static int GetRange(int min, int max)
         {
 
@@ -45,8 +47,54 @@ namespace Midterm
                 Console.ForegroundColor = ConsoleColor.DarkBlue;
                 number = GetValidInt();
             }
-            return number;
 
+            return number;
+        }
+
+
+
+        //Input Method
+        public static string GetValidString(string Choice)
+        {
+          
+            // Validates There is User Input
+            if ((string.IsNullOrEmpty(Choice)) || (string.IsNullOrWhiteSpace(Choice)))
+            {
+                Console.WriteLine("You Must Enter Words!");
+            }
+
+            return Choice;
+                
+        }
+
+
+
+
+        // Method to Continue
+        public static bool GetContinue()
+        {
+            string Continue;
+
+            //Continue Loop
+            while (true)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Do You Need Help With Anything Else?  (y/n)");
+                Continue = Console.ReadLine().ToUpper();
+                Console.WriteLine("");
+                if (Continue == "Y")
+                    return true;
+
+                if (Continue == "N")
+                    return false;
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;  // Red to Alert User Input is Invalid
+                    Console.WriteLine("Please Enter Y or N");
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;   // Dark Blue to Go Back to Original Color
+                }
+            }
         }
     }
 }
